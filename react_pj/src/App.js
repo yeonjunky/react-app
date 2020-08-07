1cb89bb3bc314b0829d12d15435d08416f781293
yeonjunky <yeonjunky@gmail.com>
2020-08-08 08:12:56 +0900

    initial commit

---
+++ react_pj/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="scrolling-wrapper">
+        <div className="container"></div>
+        <div className="container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
