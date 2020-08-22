68441e50d614c1d0fb302ffaca9e38f1cfe73f17
yeonjunky <yeonjunky@gmail.com>
2020-08-22 11:24:59 +0900

    Updates

---
+++ precache-manifest.a3a81e4da6bccf7c552ae03609ab65db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a8987f92fd3d98f5a321ff17d8d2fb",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "48a52403d3c304369507",
+    "url": "/react-app/static/css/main.f22f1704.chunk.css"
+  },
+  {
+    "revision": "ec5189feda4fe4008bbb",
+    "url": "/react-app/static/js/2.23a63c81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.23a63c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a52403d3c304369507",
+    "url": "/react-app/static/js/main.1015019c.chunk.js"
+  },
+  {
+    "revision": "a287f80ea768a61586db",
+    "url": "/react-app/static/js/runtime-main.b9f35c75.js"
+  }
+]);
