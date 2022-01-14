9a604c196b0ed0d79431251844f972f20cf1bdfe
yeonjun <yeonjunky@gmail.com>
2022-01-14 20:35:13 +0900

    class to function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import './TodoItem.css';
+import React from "react";
+import "./TodoItem.css";
 
-class TodoItem extends Component {
-  render() {
-    const { text, checked, id, onToggle, onRemove } = this.props;
+const TodoItem = ({ text, isChecked }) => {
 
-    return (
-      <div className="todo-item" onClick={() => onToggle(id)}>
-        <div className="remove" onClick={(e) => {
-          e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-          onRemove(id)}
-        }>&times;</div>
-        <div className={`todo-text ${checked && 'checked'}`}>
-          <div>{text}</div>
-        </div>
-        {
-          checked && (<div className="check-mark">✓</div>)
-        }
+  return (
+    <div className="todo-item">
+      <div className="remove">&times;</div>
+      <div className={`todo-text ${isChecked && "checked"}`}>
+        <div>{text}</div>
       </div>
-    );
-  }
-}
+      {isChecked && <div className="check-mark">✓</div>}
+    </div>
+  );
+};
 
 export default TodoItem;
