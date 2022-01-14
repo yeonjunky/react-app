b8a9ef6d2ef3205e821744fe4ca5b29118207367
yeonjun <yeonjunky@gmail.com>
2022-01-14 16:29:42 +0900

    rename files

---
+++ src/components/AddTodo.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./AddTodo.css";
+
+const Todoform = () => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="addTodo">
+      <input value={value} onChange={onChange} />
+      <button className="button">Add</button>
+    </div>
+  );
+};
+
+export default Todoform;
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Title.css";
+
+const Title = () => {
+  return <div className="title">TODO LIST</div>;
+};
+
+export default Title;
