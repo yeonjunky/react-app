59ce38ab70ead3ffccfe1e33281660fc2dd20114
yeonjun <yeonjunky@gmail.com>
2022-01-15 20:25:31 +0900

    change to function component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./TodoList.css";
 import Title from "./components/Title";
 import AddTodo from "./components/AddTodo";
 import TodoItemList from "./components/TodoItemList";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Title />
-        <AddTodo></AddTodo>
-        <TodoItemList />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Title />
+      <AddTodo></AddTodo>
+      <TodoItemList />
+    </div>
+  );
 }
 
 export default App;
