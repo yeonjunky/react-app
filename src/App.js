8e2b69032ca553d8f3401dbc2837e57c28049138
yeonjun <yeonjunky@gmail.com>
2022-01-15 20:19:07 +0900

    delete useless code

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import Title from "./components/Title";
 import AddTodo from "./components/AddTodo";
 import TodoItemList from "./components/TodoItemList";
 
-const todos = [
-  { id: 0, text: "introduce react", checked: false },
-  { id: 1, text: "hello!", checked: true },
-  { id: 2, text: "hahaha!", checked: false },
-];
-
-let id = 3;
-
 class App extends Component {
   render() {
     return (
       <div>
         <Title />
         <AddTodo></AddTodo>
-        <TodoItemList todos={todos} />
+        <TodoItemList />
       </div>
     );
   }
