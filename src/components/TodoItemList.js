4373151a53cd0f76036083f593174f0b49af3441
yeonjun <yeonjunky@gmail.com>
2022-01-16 01:31:16 +0900

    todos state, get request, change prop name

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
-const TodoItemList = ({ todos }) => {
+const TodoItemList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    const json = await fetch("http://127.0.0.1:5000/todos").then((response) =>
+      response.json()
+    );
+
+    setTodos(json);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div>
       {todos.map(({ id, text, checked }) => (
-        <TodoItem key={id} id={id} text={text} isChecked={checked}></TodoItem>
+        <TodoItem
+          key={id}
+          id={id}
+          text={text}
+          isDone={checked}
+        />
       ))}
     </div>
   );
