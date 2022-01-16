2ccf979c922bd4d1c7861efb8e71f29f7d9df81d
yeonjun <yeonjunky@gmail.com>
2022-01-16 23:11:13 +0900

    add requestOptions

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,16 +3,26 @@ import "./AddTodo.css";
 
 const AddTodo = () => {
   const [value, setValue] = useState("");
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    mode: "no-cors",
+    body: JSON.stringify({})
+  };
 
   const onChange = (event) => {
     setValue(event.target.value);
   };
 
+  const onSubmit = async (event) => {
+    await fetch("http://localhost:5000/todos");
+  };
+
   return (
-    <div className="addTodo">
+    <form className="addTodo" onSubmit={onSubmit}>
       <input value={value} onChange={onChange} />
       <button className="button">Add</button>
-    </div>
+    </form>
   );
 };
 
